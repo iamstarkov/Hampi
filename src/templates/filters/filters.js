071d516727911ea2ee9d42dc29c3226a27840ffd
Vladimir Starkov <vstarkov@yandex-team.ru>
2013-09-19 20:19:02 +0600

    added information about cases

--- src/templates/filters/filters.js
+++ src/templates/filters/filters.js
@@ -1,14 +1,36 @@
-var jade = require('jade');
+// var jade = require('jade');
 var jadefilters = module.exports = {};
+var locals = require('./../../../theme_options.json');
 
+
+/**
+ * always working fine 
+ */
 jadefilters.Posts = function(block) {
-	return '{block:Posts}'+jade.render(block)+'{/block:Posts}';
+	return '{block:Posts}'+block+'{/block:Posts}';
 };
 
 jadefilters.Audio = function(block) {
-	return '{block:Audio}'+jade.render(block)+'{/block:Audio}';
+	return '{block:Audio}'+block+'{/block:Audio}';
 };
 
 jadefilters.Video = function(block) {
-	return '{block:Video}'+jade.render(block)+'{/block:Video}';
+	return '{block:Video}'+block+'{/block:Video}';
 };
+
+/**
+ * working fine with locals, but failed with nested filters
+ */
+/*
+jadefilters.Posts = function(block) {
+	return '{block:Posts}'+jade.render(block, locals)+'{/block:Posts}';
+};
+
+jadefilters.Audio = function(block) {
+	return '{block:Audio}'+jade.render(block, locals)+'{/block:Audio}';
+};
+
+jadefilters.Video = function(block) {
+	return '{block:Video}'+jade.render(block, locals)+'{/block:Video}';
+};
+*/

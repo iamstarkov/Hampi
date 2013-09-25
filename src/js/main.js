860ff1e8abc567a7cbea37468cff1d427b277e3d
Vladimir Starkov <vstarkov@yandex-team.ru>
2013-09-26 02:52:35 +0600

    added img and js folders

---
+++ src/js/main.js
@@ -0,0 +1,15 @@
+if ($('.cells-wrap')[0]) {
+	$('.cells-wrap').mosaicflow({
+		itemSelector: '.cell',
+		minItemWidth: 304
+	});
+}
+
+if ($('.post__images-wrap')[0]) {
+	$('.post__images-wrap').mosaicflow({
+		itemSelector: 'img',
+		minItemWidth: 468
+	});
+}
+/*
+*/

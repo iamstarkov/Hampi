7c3d2325ebc8a16df194a226aa0643c6b48eaa28
Vladimir Starkov <vstarkov@yandex-team.ru>
2013-10-06 07:26:17 +0600

    added performace change to mosaicflow

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,16 @@
 if ($('.cells-wrap')[0]) {
 	$('.cells-wrap').mosaicflow({
 		itemSelector: '.cell',
-		minItemWidth: 304
+		minItemWidth: 304,
+		itemHeightCalculation: 'attribute'
 	});
 }
 
 if ($('.post__images-wrap')[0]) {
 	$('.post__images-wrap').mosaicflow({
 		itemSelector: 'img',
-		minItemWidth: 468
+		minItemWidth: 468,
+		itemHeightCalculation: 'attribute'
 	});
 }
 /*

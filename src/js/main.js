776c3c6c48b69fb522a4f32aac14801aed72f614
Vladimir Starkov <vstarkov@yandex-team.ru>
2013-10-20 04:06:56 +0600

    trying infinite scroll #4

--- src/js/main.js
+++ src/js/main.js
@@ -19,24 +19,3 @@ $('iframe').prop('width', '100%');
 $(window).resize(function() {
 	$('iframe').prop('width', '100%');
 });
-
-$(function() {
-	if ($('.content-wrap_type_index')[0]) {
-
-		// infinitescroll() is called on the element that surrounds 
-		// the items you will be loading more of
-		$('.content-wrap_type_index').infinitescroll({
-
-			// selector for the paged navigation (it will be hidden)
-			navSelector: '.navigation_type_pagination',
-
-			// selector for the NEXT link (to page 2)
-			nextSelector: '.navigation_type_pagination [rel="next"]',
-
-			// selector for all items you'll retrieve
-			itemSelector: '.post-preview',
-
-			bufferPx: document.height
-		});
-	}
-});
